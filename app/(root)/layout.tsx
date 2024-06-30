f18167811327f39f1e3ec9562497fbf6d23b4482
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-06-30 17:04:42 +0700

    create LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import NavBar from "@/components/shared/navbar/NavBar";
+import LeftSidebar from "@/components/shared/sidebar/LeftSidebar";
 import React, { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
@@ -6,9 +7,9 @@ const Layout = ({ children }: PropsWithChildren) => {
     <main className="background-light850_dark100 relative">
       <NavBar />
       <div className="flex">
-        LeftSide
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-          <div className="mx-auto w-full max-w-5xl">{children}</div>
+          <div className="mx-auto h-[2000px] w-full max-w-5xl">{children}</div>
         </section>
         RightSide
       </div>
