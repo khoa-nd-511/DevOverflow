9f01782daa9a465fe45761bc2af3e487a9d3edeb
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-06-30 23:52:22 +0700

    created LocalSearch comp

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,29 @@
-import { UserButton } from "@clerk/nextjs";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <UserButton />;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between sm:flex-row">
+        <h1 className="h1-bold text-dark100_light900 max-sm:mt-6">
+          All Questions
+        </h1>
+
+        <Link href="/ask-question">
+          <Button className="primary-gradient min-h-[46px] w-full px-4 py-3 text-light-900">
+            Ask A Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch placeholder="Search questions" />
+        Filters
+      </div>
+    </>
+  );
 };
 
 export default page;
