d30238b836f833093fc257943ba601194b796b07
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-07-01 14:29:13 +0700

    create question form with title and wysiwyg editor for description

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,16 @@
+import QuestionForm from "@/components/forms/QuestionForm";
 import React from "react";
 
-const page = () => {
-  return <div>Ask Question</div>;
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
---
+++ components/forms/QuestionForm.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { askQuestionFormSchema } from "@/lib/validations";
+
+const QuestionForm = () => {
+  const editorRef = useRef<Editor>();
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof askQuestionFormSchema>>({
+    resolver: zodResolver(askQuestionFormSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      tags: [],
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof askQuestionFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col gap-10 space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-1">
+                <Input
+                  className="no-focus paragraph-regular light-border-2 background-light900_dark300 text-dark300_light700"
+                  placeholder="Enter question title..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Description <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-1">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(_evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  initialValue=""
+                  init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                    ],
+                    toolbar:
+                      "undo redo | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist",
+                    content_style: "body { font-family:Inter; font-size:16px }",
+                  }}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-1">
+                {/* Add editor */}
+                <Input
+                  className="no-focus paragraph-regular light-border-2 background-light900_dark300 text-dark300_light700"
+                  placeholder="Enter up to 3 tags that are related to the question..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="paragraph-medium min-h-[46px] rounded-lg bg-primary-500 text-light-900"
+        >
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default QuestionForm;
