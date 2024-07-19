91b4376e6f417285c7143594fd2571cd2ee1f902
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-07-19 23:19:08 +0700

    fixed loading state of tag page

--- app/(root)/tags/[id]/loading.tsx
+++ app/(root)/tags/[id]/loading.tsx
@@ -1,4 +1,3 @@
-import CTAButton from "@/components/shared/CTAButton";
 import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
@@ -6,14 +5,10 @@ const loading = () => {
     return (
         <>
             <div className="flex w-full flex-col-reverse justify-between sm:flex-row">
-                <h1 className="h1-bold text-dark100_light900 max-sm:mt-6">
-                    Saved Questions
-                </h1>
-
-                <CTAButton label="Ask a question" href="/ask-question" />
+                <Skeleton className="h-[42px] w-[100px] rounded-lg" />
             </div>
 
-            <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+            <div className="mt-10 flex justify-between gap-5 max-sm:flex-col sm:items-center">
                 <Skeleton className="h-[56px] w-full rounded-xl" />
             </div>
 
